238962f977062edf10ba00548af1f95bf05fc268
Shaun Vambe <you@example.com>
2025-10-13 15:45:53 +1100

    Manage page added

---
+++ manage.php
@@ -0,0 +1,27 @@
+<?php
+
+$currentPage = 'jobs';
+$pageTitle = 'JSM Jobs Applications Page';
+$pageDescription = 'Careers page for JSM website';
+$pageHeading = 'Careers - Positions Available';
+
+include 'header.inc';
+include 'nav.inc';
+?>
+
+<?php
+    // Include database settings
+    require_once 'settings.php';
+    // Create connection
+    $conn = new mysqli($host, $user, $pwd, $sql_db);
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+if (isset($_GET['model'])) {
+    $model = mysqli_real_escape_string($conn, $_GET['model']);
+    $sql = "SELECT * FROM cars WHERE model LIKE '%$model%'";
+    $result = mysqli_query($conn, $sql);
+}
+
+?>
