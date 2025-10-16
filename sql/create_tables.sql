159075c4a04c179dbc7d395e3672cf137d3d0d32
Morgan Hopkins <106188591@student.swin.edu.au>
2025-10-16 14:18:35 +1100

    create table script seems to work now

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,90 +1,90 @@
 -- JOBS TABLE
 
-DROP TABLE Jobs;
+DROP TABLE IF EXISTS Jobs;
 
 CREATE TABLE Jobs(
-		RefNo		VarChar(5) PRIMARY KEY NOT NULL;
-	);
+    RefNo VARCHAR(5) PRIMARY KEY NOT NULL
+);
 
-INSERT INTO		Jobs(RefNo)
-VALUES			('ab123');
+INSERT INTO Jobs(RefNo)
+VALUES ('ab123');
 
 -- USER TABLE
 
-DROP TABLE User;
+DROP TABLE IF EXISTS User;
 
-CREATE TABLE	User (
-	ID 		        INT PRIMARY KEY AUTO_INCREMENT,
-    Username        VARCHAR(128) NOT NULL UNIQUE,
-    Password        Varchar(255) NOT NULL,
-    Name            Varchar(128),
-    Member          Boolean,
-    login_attempts  INT,
-    lockout_time    DATETIME
+CREATE TABLE User (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(128) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Name VARCHAR(128),
+    Member BOOLEAN,
+    login_attempts INT,
+    lockout_time DATETIME
 );
 
-INSERT INTO		User(Username, Password, Name, Member)
-VALUES 			('jtaylor', 'swinburne', 'Jonathon', True);
+INSERT INTO User(Username, Password, Name, Member)
+VALUES ('jtaylor', 'swinburne', 'Jonathon', TRUE);
 
-INSERT INTO		User(Username, Password, Name, Member)
-VALUES 			('mhopkins', 'swinburne', 'Morgan', True);
+INSERT INTO User(Username, Password, Name, Member)
+VALUES ('mhopkins', 'swinburne', 'Morgan', TRUE);
 
-INSERT INTO		User(Username, Password, Name, Member)
-VALUES 			('svambe', 'swinburne', 'Shaun', True);
+INSERT INTO User(Username, Password, Name, Member)
+VALUES ('svambe', 'swinburne', 'Shaun', TRUE);
 
-INSERT INTO		User(Username, Password, Name, Member)
-VALUES 			('admin', 'admin', 'Administrator', False);
+INSERT INTO User(Username, Password, Name, Member)
+VALUES ('admin', 'admin', 'Administrator', FALSE);
 
 -- ABOUT TABLE
 
-DROP TABLE About;
+DROP TABLE IF EXISTS ABOUT; 
 
-CREATE TABLE	ABOUT(
-	AID			INT PRIMARY KEY AUTO_INCREMENT,
-	Task		VARCHAR(128) NOT NULL,
-	Description	Varchar(256) NOT NULL
-)
+CREATE TABLE ABOUT(
+    AID INT PRIMARY KEY AUTO_INCREMENT,
+    Task VARCHAR(128) NOT NULL,
+    Description VARCHAR(256) NOT NULL
+);
 
 -- EOI TABLE
-
-DROP TABLE EOI;
+DROP TABLE IF EXISTS EOI;
 
 CREATE TABLE EOI(
-	EOIno 		INT PRIMARY KEY AUTO_INCREMENT,
-	RefNo 		Varchar(5) NOT NULL,
-	ID 	INT NOT NULL,
-	ApplyDate 	DATE NOT NULL DEFAULT CURRENT_DATE,
-	FirstName	Varchar(20) NOT NULL,
-	LastName	Varchar(20) NOT NULL,
-	DOB			DATE NOT NULL,
-	Gender		Varchar(2) NOT NULL,
-	Address		Varchar(40) NOT NULL,
-	Suburb		Varchar(40) NOT NULL,
-	Postcode	INT CHECK (Postcode BETWEEN 0 AND 9999) NOT NULL,
-	State		Varchar(3) NOT NULL,
-	Email		Varchar(40) NOT NULL,
-	PhoneNo		Varchar(12) NOT NULL,
-	Skills		Varchar(128),
-	OtherSkills	Varchar(1024),
-	Status		ENUM('New', 'Current', 'Final') DEFAULT 'New',
-	FOREIGN KEY	(RefNo) REFERENCES Jobs(RefNo) ON DELETE CASCADE,
-	FOREIGN KEY (ID) REFERENCES User(ID) ON DELETE CASCADE,
-	UNIQUE(UserID, RefNo)
-	);
-	
-INSERT INTO 	EOI(RefNo, Username, FirstName, LastName, DOB, Gender, Address,
-				Suburb, Postcode, State, Email, Phoneno, Skills, OtherSkills)
-VALUES			('ab123', 'realslimshady', 'Slim', 'Shady', CURRENT_DATE, 'M', '8 Mile', 'Compton', 3196, 'VIC',
-				'slim@shady.com', '0418696969', 'Listening', 'Rapping about yo mama');
+    EOIno INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    RefNo VARCHAR(5) NOT NULL,
+    ID INT NOT NULL,
+    ApplyDate DATE NOT NULL DEFAULT CURRENT_DATE,
+    FirstName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(2) NOT NULL,
+    Address VARCHAR(40) NOT NULL,
+    Suburb VARCHAR(40) NOT NULL,
+    Postcode INT NOT NULL,
+    State VARCHAR(3) NOT NULL,
+    Email VARCHAR(40) NOT NULL,
+    PhoneNo VARCHAR(12) NOT NULL,
+    Skills VARCHAR(128),
+    OtherSkills VARCHAR(1024),
+    Status ENUM('New', 'Current', 'Final') DEFAULT 'New',
+    FOREIGN KEY (RefNo) REFERENCES Jobs(RefNo) ON DELETE CASCADE,
+    FOREIGN KEY (ID) REFERENCES User(ID) ON DELETE CASCADE,
+    UNIQUE(ID, RefNo),
+    CONSTRAINT chk_postcode CHECK (Postcode BETWEEN 0 AND 9999)
+);
+
+INSERT INTO EOI(RefNo, ID, FirstName, LastName, DOB, Gender, Address,
+    Suburb, Postcode, State, Email, PhoneNo, Skills, OtherSkills)
+VALUES ('ab123', 1, 'Slim', 'Shady', CURDATE(), 'M', '8 Mile', 
+    'Compton', 3196, 'VIC', 'slim@shady.com', '0418696969', 'Listening', 'Rapping about yo mama');
 
 -- CONTRIBUTION TABLE
 
-DROP TABLE Contribution;
+DROP TABLE IF EXISTS Contribution;
 
-CREATE TABLE	Contribution(
-	CID		INT PRIMARY KEY AUTO_INCREMENT,
-	ID		INT NOT NULL,
-	AID		INT NOT NULL,
-	FOREIGN KEY (ID) REFERENCES User(ID) ON DELETE CASCADE;
-	FOREIGN KEY (AID) REFERENCES About(AID) ON DELETE CASCADE;
-)
+CREATE TABLE Contribution(
+    CID INT PRIMARY KEY AUTO_INCREMENT,
+    ID INT NOT NULL,
+    AID INT NOT NULL,
+    FOREIGN KEY (ID) REFERENCES User(ID) ON DELETE CASCADE,
+    FOREIGN KEY (AID) REFERENCES About(AID) ON DELETE CASCADE
+);
